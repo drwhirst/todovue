1d9080d81b04227b2edd773d97838e39a4640ecd
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-04-30 19:04:25 -0400

    building out the add todo functionality

---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class='ui basic content center aligned segment'>
+        <button class='ui basic button icon' v-on:click="openForm" v-show="!isCreating">
+            <i class='plus icon'></i>
+        </button>
+        <div class='ui centered card' v-show="isCreating">
+            <div class='content'>
+                <div class='ui form'>
+                    <div class='field'>
+                        <label>Title</label>
+                        <input v-model="titleText" type='text' ref='title' defaultValue=''>
+                    </div>
+                    <div class='field'>
+                        <label>Project</label>
+                        <input v-model=''
+
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -11,6 +11,9 @@
           <span class='right floated edit icon' v-on:click="showForm">
             <i class='edit icon'></i>
           </span>
+          <span class='right floated trash icon' v-on:click="deleteTodo(todo)">
+            <i class='trash icon'></i>
+          </span>
         </div>
     </div>
     <div class="content" v-show="isEditing">
@@ -30,10 +33,10 @@
             </div>
         </div>
     </div>
-    <div class='ui bottom attached green basic button' v-show="todo.done">
+    <div class='ui bottom attached green basic button' v-show="!isEditing &&todo.done" disabled>
         Completed
     </div>
-    <div class='ui bottom attached red basic button' v-show="!todo.done">
+    <div class='ui bottom attached red basic button' v-show="!todo.done  && !isEditing">
         Complete
     </div>
 </div>
@@ -53,7 +56,10 @@
             },
             hideForm() {
                 this.isEditing = false;
-            }
+            },
+            deleteTodo(todo) {
+                this.$emit('delete-todo', todo);
+            },
         }
     };
 </script>
